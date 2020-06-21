55b0c237770f1def61b52f4d93ecd3573899d6f7
Kirill <61191094+Karen-art-tech@users.noreply.github.com>
2020-06-21 22:51:46 +0300

    Test update (try1)#4

--- Test/java/TestMain.java
+++ Test/java/TestMain.java
@@ -23,7 +23,47 @@ import org.junit.Assert;
             Assert.assertEquals(t1.mRoot.mLeft.mValue, 10);
             Assert.assertEquals(t1.mRoot.mRight.mValue, 56);
         }
+ @Test
+    public void testDefaultConstructor() {
+        Main t1 = new Main();
+        Assert.assertNull(t1.mRoot);
+    }
 
+    @Test
+    public void testIntegerConstructor() {
+        Main t1 = new Main(1);
+        Assert.assertNotNull(t1.mRoot);
+    }
+
+
+    @Test
+    public void testEqualTreesEqualHashCodes() {
+        Main t1 = new Main(10);
+        t1.insert(2, 12);
+        Main t2 = new Main(10);
+        t2.insert(2, 12);
+        Assert.assertEquals(t1.hashCode(), t2.hashCode());
+    }   
+
+
+    @Test
+    public void testToStringEmpty() {
+        Main t1 = new Main();
+        Assert.assertEquals("[]", t1.toString());
+    }
+
+    @Test
+    public void testToStringSingleNode() {
+        Main t1 = new Main(1);
+        Assert.assertEquals("[1]", t1.toString());
+    }
+
+    @Test
+    public void testToStringManyNodes() {
+        Main t1 = new Main(1);
+        t1.insert(12, 56, 7, 2, 1);
+        Assert.assertEquals("[1, 1, 2, 7, 12, 56]", t1.toString());
+    }
         @org.junit.Test
         public void testSingleRotateRight() {
             Main t1 = new Main(10);
